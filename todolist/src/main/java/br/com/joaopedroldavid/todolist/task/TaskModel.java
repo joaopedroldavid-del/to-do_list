001cc9d8d4250a3d951c958b2fc4147ff8aaf795
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-04-28 15:01:47 -0300

    Finish project

--- todolist/src/main/java/br/com/joaopedroldavid/todolist/task/TaskModel.java
+++ todolist/src/main/java/br/com/joaopedroldavid/todolist/task/TaskModel.java
@@ -32,5 +32,12 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("O campo de título deve conter no máximo 50 caracteres");
+        }    
+        this.title = title;
+    }
+
 }
     
